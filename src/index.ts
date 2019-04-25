537fd991c35385f92e8b316934220362db3cfa3d
Josh Goldberg <josh@fullscreenmario.com>
2019-04-24 21:05:30 -0400

    Removed unused admin-mongo thing; went back to port 3000

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,5 @@ app.use(async context => {
     context.body = 'Hello World! :D :D';
 });
 
-console.log('Listening on 4000...');
-app.listen(4000);
+console.log('Listening on 3000...');
+app.listen(3000);
